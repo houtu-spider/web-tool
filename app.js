20c69a1cc17e3b902e607ffce4724669a57b6476
张银川 <zhangyinchuan3783@dayukeji.com>
2024-11-08 18:34:05 +0800

    架构调整

---
+++ app.js
@@ -0,0 +1,50 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const serverFavicon = require('serve-favicon')
+
+const indexRouter = require('./routes/index');
+const menu = require('./routes/menu')
+const detail = require('./routes/detail')
+const timestamp_covert = require('./routes/detail/timestamp_covert')
+const send_me_message = require('./routes/detail/send_me_message')
+const spider_chat = require('./routes/detail/spider_chat')
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(serverFavicon(path.join(__dirname, './public/icons/favicon.ico')));
+
+app.use('/', indexRouter);
+// 详情
+app.use('/detail', detail);
+// 菜单页
+app.use('/menu', menu);
+// 时间戳转换
+app.use('/timestamp_covert', timestamp_covert);
+// 发送消息
+app.use('/send_me_message', send_me_message);
+// spider_chat
+app.use('/spider_chat', spider_chat);
+
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ routes/detail/send_me_message.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', function (req, res, next) {
+    res.render('detail/send_me_message')
+})
+
+router.get('/send_message', function (req, res, next) {
+    let ip = getClientIp(req);
+    let host = req.hostname;
+    let message = req.query.web_message;
+    message = `sender: ${ip} ${host}\n` + message;
+    return send_msg(message);
+})
+
+async function send_msg(message) {
+    if (!message || !message.length) {
+        return false;
+    }
+
+    let data = {
+        msg_type: "text",
+        content: {
+            text: message,
+        },
+    };
+    await fetch("https://open.feishu.cn/open-apis/bot/v2/hook/14451567-0ab4-482f-ad7e-42dcfd724617", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    console.log("send_msg Success")
+    return true;
+}
+
+function getClientIp(req) {
+    return req.headers['x-forwarded-for'] ||
+        req.ip ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress ||
+        'un catch';
+}
+
+function ipv6ToV4(ip) {
+    if(ip.split(',').length>0){
+        ip = ip.split(',')[0]
+    }
+    ip = ip.substr(ip.lastIndexOf(':')+1,ip.length);
+    return ip
+}
+
+module.exports = router;
